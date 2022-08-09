8cdd99836945eea3b1c0b64c155b8d95576cf80e
Mustafa Burak Gürener <mb.gurener@gmail.com>
2022-08-09 22:34:55 +0300

    Add project files.

---
+++ FacadeLayer/Connection/DbConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace FacadeLayer.Connection
+{
+    internal class DbConnection
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-T9KCH5P;Initial Catalog=DbAkademiFilmDizi;Integrated Security=True;");
+    }
+}
